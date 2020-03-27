983a97d73d6511ef4ed51e48fdce32d065619663
nateg <nategold7@gmail.com>
2020-03-26 20:53:16 -0400

    initial commit

---
+++ src/main/java/com/SP1CYH0T/github/jojomod/JojoMod.java
@@ -0,0 +1,8 @@
+package com.SP1CYH0T.github.jojomod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(JojoMod.MODID)
+public class JojoMod {
+    public static final String MODID = "jojomod";
+}
